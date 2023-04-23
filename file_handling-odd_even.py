886c5a741534c4487457b308933cbf36d69eb91f
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-23 13:15:32 +0800

    pseudocode

---
+++ file_handling-odd_even.py
@@ -0,0 +1,9 @@
+# File handling - Even and Odd numbers
+# Phoebe Rhone L. Gangoso | BSCPE 1-4
+
+# open numbers.txt (read), open even.txt (append), open odd.txt (append)
+    # read numbers.txt line by line
+        # if even
+            # append even numbers to even.txt
+        # if odd
+            # append odd numbers to odd.txt
