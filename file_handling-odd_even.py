32f6db9fe243c4b8a5509635c7760aca0389c543
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-23 13:22:59 +0800

    printing numbers

--- file_handling-odd_even.py
+++ file_handling-odd_even.py
@@ -2,7 +2,11 @@
 # Phoebe Rhone L. Gangoso | BSCPE 1-4
 
 # open numbers.txt (read), open even.txt (append), open odd.txt (append)
+with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd:
     # read numbers.txt line by line
+    for line in input_file:
+        int_line = int(line)
+        print(int_line)
         # if even
             # append even numbers to even.txt
         # if odd
