68a9eb24a1dd372a1efed70b0bfc639e6a2da872
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-23 13:47:40 +0800

    add labels

--- file_handling-odd_even.py
+++ file_handling-odd_even.py
@@ -3,6 +3,8 @@
 
 # open numbers.txt (read), open even.txt (append), open odd.txt (append)
 with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd:
+    output_even.write("EVEN NUMBERS:\n")
+    output_odd.write("ODD NUMBERS:\n")
     # read numbers.txt line by line
     for line in input_file:
         int_line = int(line)
